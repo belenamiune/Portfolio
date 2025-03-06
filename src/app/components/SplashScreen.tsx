7f2cbfbb47894555188bc953f5d04ea09c6c7023
belenamiune <amiunebelen88@gmail.com>
2025-03-06 14:12:45 -0300

    Config

--- src/app/components/SplashScreen.tsx
+++ src/app/components/SplashScreen.tsx
@@ -1,7 +1,12 @@
-import { useEffect } from "react";
+"use client"; 
+
+import { useEffect, useState } from "react";
 
 const SplashScreen = ({ onFinish }: { onFinish: () => void }) => {
+  const [isClient, setIsClient] = useState(false);
+
   useEffect(() => {
+    setIsClient(true);
     const timer = setTimeout(() => {
       onFinish();
     }, 3000); 
@@ -9,9 +14,11 @@ const SplashScreen = ({ onFinish }: { onFinish: () => void }) => {
     return () => clearTimeout(timer); 
   }, [onFinish]);
 
+  if (!isClient) return null;
+
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800 animate-pulse">Bienvenido</h1>
+      <h1 className="text-4xl font-bold text-gray-800 animate-pulse">Welcome</h1>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,31 @@
-"use client";
+
+
 import { useState } from "react";
-import SplashScreen from "@/app/components/SplashScreen";
-import Hero from "@/app/pages/Hero";
-import Portfolio from "@/app/pages/Portfolio";
-import About from "@/app/pages/About";
-import Contact from "@/app/pages/Contact";
-import Navbar from "@/app/components/Navbar";
+import dynamic from "next/dynamic";
+
+const SplashScreen = dynamic(() => import("@/app/components/SplashScreen"), { ssr: false });
+const Navbar = dynamic(() => import("@/app/components/Navbar"));
+const Hero = dynamic(() => import("@/app/pages/Hero"));
+const Portfolio = dynamic(() => import("@/app/pages/Portfolio"));
+const About = dynamic(() => import("@/app/pages/About"));
+const Contact = dynamic(() => import("@/app/pages/Contact"));
 
 export default function Home() {
   const [isSplashVisible, setIsSplashVisible] = useState(true);
+
   return (
-      isSplashVisible ? (
+    <>
+      {isSplashVisible ? (
         <SplashScreen onFinish={() => setIsSplashVisible(false)} />
       ) : (
-      <>
-      
-      <Navbar /> 
-      <Hero />
-      <About />
-      <Portfolio />
-      <Contact />
+        <>
+          <Navbar />
+          <Hero />
+          <About />
+          <Portfolio />
+          <Contact />
+        </>
+      )}
     </>
-      )
   );
 }
